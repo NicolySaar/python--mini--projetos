8a14ee7a26b79a33546fc04010b166eae94226ce
NicolySaar <saarnicoly@gmail.com>
2026-09-03 09:55:39 -0300

    Add extensions solution

---
+++ cs50/pset1/file.py
@@ -0,0 +1,16 @@
+file = input("File name: ").lower().strip()
+
+if file.endswith('.jpg'):
+    print('image/jpeg')
+elif file.endswith('.gif'):
+    print('image/gif')
+elif file.endswith('.pdf'):
+    print('application/pdf')
+elif file.endswith('.zip'):
+    print('application/zip')
+elif file.endswith('.png'):
+    print('image/png')
+elif file.endswith('.txt'):
+    print('text/plain')
+else:
+    print('application/octet-stream')
